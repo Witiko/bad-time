35d90031d838ca5457d1c13252fc847b97ce3d8f
Vít Novotný <witiko@mail.muni.cz>
2016-10-26 19:51:36 +0200

    Added gimp-export.sh directly to the repository.

--- gimp-export.sh
+++ gimp-export.sh
@@ -1,1 +1,18 @@
-../Psychedelic Democracy/gimp-export.sh
+#!/bin/sh
+# Exports project "$1" as a PNG file "$2".
+gimp --no-interface --batch '
+(let*
+  (
+    (input "'"$1"'")
+    (output "'"$2"'")
+    (image (car
+      (gimp-file-load RUN-NONINTERACTIVE input input)))
+    (drawable (car
+      (gimp-image-merge-visible-layers image CLIP-TO-IMAGE)))
+  )
+  (gimp-layer-resize-to-image-size drawable)
+  (file-png-save RUN-NONINTERACTIVE image drawable output output 0 9 1 0 0 0 1)
+  (gimp-image-delete image)
+  (gimp-quit 0)
+)
+'
